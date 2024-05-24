1bececaf5935c27285366713bb072acd191db6c1
Exagor <alexandre.achten@gmail.com>
2024-05-24 23:08:18 +0200

    try define model

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 import gurobipy as gp
+from gurobipy import GRB
 import numpy as np
 import pandas as pd
 
 data_dir_path = "data/"
-data_revenue = pd.read_csv(data_dir_path + "small-r.csv")
-data_prob = pd.read_csv(data_dir_path + "small-mu.csv")
+data_revenue = pd.read_csv(data_dir_path + "small-r.csv",sep=';')
+data_prob = pd.read_csv(data_dir_path + "small-mu.csv",sep=';')
+print(data_revenue.head())
+print(data_revenue.shape)
 
 def read_data(data_revenue, data_prob):
     return data_revenue, data_prob
@@ -15,12 +18,38 @@ def assortment_planning(data_revenue, data_prob, time_limit=60):
     model.setParam('OutputFlag', 1) # 1 for verbose, 0 for silent
 
     # define variables
+    I = data_revenue.shape[0] # set of products
+
+    x = {} # decision variable (1 if product i is selected, 0 otherwise)
+    for i in range(data_revenue.shape[0]):
+        x[i] = model.addVar(vtype=GRB.BINARY, name="x[%d]" % i)
+
+    r = {} #revenue of product i
+    for i in range(data_revenue.shape[0]):
+        r[i] = model.addVar(vtype=GRB.CONTINUOUS, name="r[%d]" % i) # or %s i don't know
+
+    mu = {} # mean utility of product i
+    for i in range(data_prob.shape[0]):
+        mu[i] = model.addVar(vtype=GRB.CONTINUOUS, name="mu[%d]" % i)
+
+    S = {} #set of choice for the customer
+    # define probability
+
+    P = {} # probability of customer choosing product i depending on the assortment
+    for i in range(data_prob.shape[0]):
+        P[i] = model.addVar(vtype=GRB.CONTINUOUS, name="P[%d]" % i)
 
     # define constraints
 
+    model.update()
+
+    # define objective function
+    model.setObjective(r[0]*P[0] + gp.quicksum(r[i]*P[i]), GRB.MAXIMIZE)
 
     return model
 
 if __name__ == "__main__":
-    print("Hello World !")
+    print("Hi word")
+    # assortment_model = assortment_planning(data_revenue, data_prob)
+    # assortment_model.optimize()
 
