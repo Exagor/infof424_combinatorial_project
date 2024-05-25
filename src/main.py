325f71e1550960808cea2d8989fd286758431b90
Exagor <alexandre.achten@gmail.com>
2024-05-25 18:33:32 +0200

    Loop on all instances

--- src/main.py
+++ src/main.py
@@ -1,16 +1,20 @@
 import pandas as pd
 from APL import *
 from APC_MILP import *
+from utilities import *
 
 data_dir_path = "data/"
 size_name = "small" #or medium or large
 instances_data_revenue = pd.read_csv(data_dir_path + size_name+"-r.csv",sep=';',header=None)
 instances_data_mu = pd.read_csv(data_dir_path + size_name + "-mu.csv",sep=';',header=None)
 
-def read_instance(instances_data_revenue, instances_data_mu, instance=0):
-    data_revenue = instances_data_revenue.iloc[:,instance] #we use iloc to select the column
-    data_mu = instances_data_mu.iloc[:,instance]
-    return data_revenue.tolist(), data_mu.tolist()
+def APL_instances(instances_data_revenue, instances_data_mu):
+    for i in range(len(instances_data_revenue.columns)):
+        data_revenue, data_mu = read_instance(instances_data_revenue, instances_data_mu, instance=i)
+        apl_model = APL_model(data_revenue, data_mu)
+        apl_model.optimize()
+        print("Optimal value:", apl_model.objVal)
+        save_result(apl_model, "results/results_APL.csv", instance_nb=i, size_instance=len(data_revenue))
 
 if __name__ == "__main__":
     # AP-L
@@ -19,14 +23,14 @@ if __name__ == "__main__":
     apl_model.optimize()
     print("Optimal value:", apl_model.objVal)
     print("Optimal solution:")
-    for v in apl_model.getVars():
-        print(v.varName, v.x)
+    # for v in apl_model.getVars():
+    #     print(v.varName, v.x)
     
     # APC-MILP
     apc_milp_model = APC_MILP(data_revenue, data_mu)
     apc_milp_model.optimize()
     print("Optimal value:", apc_milp_model.objVal)
     print("Optimal solution:")
-    for v in apc_milp_model.getVars():
-        print(v.varName, v.x)
+    # for v in apc_milp_model.getVars():
+    #     print(v.varName, v.x)
 
---
+++ src/utilities.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import os
+import csv
+
+
+def read_instance(instances_data_revenue, instances_data_mu, instance=0):
+    data_revenue = instances_data_revenue.iloc[:,instance] #we use iloc to select the column
+    data_mu = instances_data_mu.iloc[:,instance]
+    return data_revenue.tolist(), data_mu.tolist()
+
+def save_result(model, filename, instance_nb, size_instance):
+    with open(filename, mode='a',newline="") as file:
+        writer = csv.writer(file, delimiter=',')
+        writer.writerow([instance_nb, size_instance, model.objVal])
