4acdd88df5993e9bcd30d0e0d58ef6d2ec418d24
Jboistel <jboistel@gmail.com>
2024-06-02 15:41:05 +0200

    lagrangian

--- src/APC_IP.py
+++ src/APC_IP.py
@@ -1,52 +1,79 @@
 import numpy as np
+import gurobipy as gp
+import matplotlib.pyplot as plt
 
 
-def lagrangian_relaxation(n, mu, r, r0, lambda_val, p):
-    # Adjust marginal utilities for the Lagrangian multiplier
-    utilities = r * np.exp(mu) - lambda_val * np.exp(mu)
+def lagrangian_relaxation(n, mu, r, lambda_val, p):
+    model = gp.Model("AP-L(λ)")
+    model.setParam('OutputFlag', 0) # 1 for verbose, 0 for silent
+
+    # Define variables
+    #y0 = model.addVar(lb=0, vtype=gp.GRB.CONTINUOUS, name="y0")
+    y = model.addVars(n,lb=0, vtype=gp.GRB.CONTINUOUS, name="y")
+
+    # Define constraints
+    model.addConstr(y[0] + gp.quicksum(y[i] for i in range(n)) == 1, name="c0")
+    for i in range(1, n):
+        model.addConstr(y[i] <= y[0] * np.exp(mu[i]), name="c1_" + str(i))
     
-    # Select the top-p items based on marginal utilities
-    sorted_indices = np.argsort(-utilities)
-    selected_indices = sorted_indices[:p]
-    
-    # Create y based on selected indices
-    y = np.zeros(n)
-    for i in selected_indices:
-        y[i] = np.exp(mu[i])
+    # Set objective
+    model.setObjective(((r[0] + (lambda_val * p)) * y[0]) + gp.quicksum((((r[i]*np.exp(mu[i]) - lambda_val)/np.exp(mu[i])) * y[i]) for i in range(1, n)), gp.GRB.MAXIMIZE)
 
-    y0 = 1 / (1 + np.sum(y))
-    y = y*y0
-    
-    obj_val = (r0 + np.sum(y * r * np.exp(mu)))/(1 + np.sum(y * np.exp(mu))) - lambda_val * (np.sum(y) - p * y0)
-    return y, y0, obj_val, np.sum(y) - p * y0
+    model.update()
 
+    model.optimize()
 
+    y0_val = y[0].X
+    y_val = [y[i].X for i in range(n)]
+    obj_val = model.objVal
 
-def lagrangian_dichotomic_search(mu, r, r0, p, n, max_iter=100, tol=1e-6):
-    best_obj_val = -np.inf
+    return y_val, y0_val, obj_val
+
+def is_feasible(y, p):
+    return sum(1 for yi in y if yi > 0) <= p
+
+def lagrangian_dichotomic_search(mu, r, p, n, max_iter=100, tol=1e-6):
+    best_obj_val = np.inf
     best_y = None
     best_y0 = None
+    obj_vals = []
+    lambdas = []
 
     # Bounds for lambda
     lambda_low = 0
-    lambda_up = (max(r) - r0)/p
+    lambda_up = (r[1] - r[0])/p
 
     for _ in range(max_iter): # index of loop is not used
         lambda_val = (lambda_low + lambda_up) / 2
-        y, y0, obj_val, violation = lagrangian_relaxation(n, mu, r, r0, lambda_val, p)
+        y, y0, obj_val = lagrangian_relaxation(n, mu, r, lambda_val, p)
 
-        if obj_val > best_obj_val:
-            best_obj_val = obj_val
-            best_y = y
-            best_y0 = y0
+        obj_vals.append(obj_val)
+        lambdas.append(lambda_val)
+        if is_feasible(y, p):
+            if obj_val < best_obj_val:
+                best_obj_val = obj_val
+                best_y = y
+                best_y0 = y0
 
-        if(np.abs(violation) < tol):
-            break
-
-        if violation > 0:
-            lambda_low = lambda_val
-
-        else:
+        if obj_val < lagrangian_relaxation(n, mu, r, lambda_val + tol, p)[2]:
             lambda_up = lambda_val
 
-        return best_y, best_y0, best_obj_val
+        else:
+            lambda_low = lambda_val
+
+        if(lambda_up - lambda_low < tol):
+            break
+
+    print(lambdas)
+    print(obj_vals)
+    plot_bounds(obj_vals, lambdas)
+
+    return best_y[1:], best_y0, best_obj_val
+
+def plot_bounds(obj_vals, lambdas):
+    
+    plt.plot(lambdas, obj_vals)
+    plt.xlabel("λ")
+    plt.ylabel("Objective value")
+    plt.title("Objective value as a function of λ")
+    plt.show()
--- src/main.py
+++ src/main.py
@@ -50,19 +50,20 @@ if __name__ == "__main__":
     # size_name = "data/small"
     # instances_data_revenue = pd.read_csv(data_dir_path + size_name+"-r.csv",sep=';',header=None)
     # instances_data_mu = pd.read_csv(data_dir_path + size_name + "-mu.csv",sep=';',header=None)
-    # data_revenue, data_mu = read_instance(instances_data_revenue, instances_data_mu, instance=0)
+     data_revenue, data_mu = read_instance(instances_data_revenue, instances_data_mu, instance=0)
 
     # run all instances
-    run_all_sizes_and_instances(AP=False, APL=False, APCMILP=False, delete_result=False) # Run all instances and don't delete previous results
+    # run_all_sizes_and_instances(AP=False, APL=False, APCMILP=False, delete_result=False) # Run all instances and don't delete previous results
 
     # APC-IP
-    # r = data_revenue[1:]
-    # mu = data_mu[1:]
-    # langrangian_combination, langrangian_y0, langrangian_obj_val = lagrangian_dichotomic_search(mu, r, data_revenue[0], 10, len(r))
-    # print("Lagrangian relaxation:")
-    # print("Optimal value:", langrangian_obj_val)
-    # print("Optimal solution:", langrangian_combination)
-    # print("y0:", langrangian_y0)
+     #r = data_revenue[1:]
+     #mu = data_mu[1:]
+     N = len(data_revenue)
+     langrangian_combination, langrangian_y0, langrangian_obj_val = lagrangian_dichotomic_search(data_mu, data_revenue, N, N)
+     print("Lagrangian relaxation:")
+     print("Optimal value:", langrangian_obj_val)
+     print("Optimal solution:", langrangian_combination)
+     print("y0:", langrangian_y0)
     
     # AP-L
     # apl_model = APL_model(data_revenue, data_mu)
